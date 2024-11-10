30c3c78668d7b95b03d024748aef28dfa4910564
TaiyoMaruyama <stickytomatobaby0501@gmail.com>
2024-11-10 22:53:58 +0900

    Lucideをアイコンとして使用

--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -1,7 +1,17 @@
+import { Avatar, AvatarFallback } from '@radix-ui/react-avatar'
+import { Menu } from 'lucide-react'
+import { Button } from '../shadcn/button'
+
 const Header = () => {
   return (
-    <header className='h-14 w-full bg-red-900 flex items-center justify-center'>
+    <header className='h-14 w-full bg-red-900 flex items-center justify-between px-8'>
+      <Button variant='outline'>
+        <Menu className='text-white' />
+      </Button>
       <h1 className='text-white font-bold text-xl'>シネマ予約</h1>
+      <Avatar>
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
     </header>
   )
 }
