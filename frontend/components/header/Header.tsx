035205147eece987bc649b9ac45d1dd3b09f5aee
TaiyoMaruyama <stickytomatobaby0501@gmail.com>
2024-11-10 22:41:18 +0900

    ヘッダーのベースを作成

---
+++ frontend/components/header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className='h-14 w-full bg-red-900 flex items-center justify-center'>
+      <h1 className='text-white font-bold text-xl'>シネマ予約</h1>
+    </header>
+  )
+}
+
+export default Header
