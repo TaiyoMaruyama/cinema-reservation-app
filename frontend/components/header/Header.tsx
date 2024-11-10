4b9e0766206abf3fc12a76a33ab5d8ea5b772fd9
TaiyoMaruyama <stickytomatobaby0501@gmail.com>
2024-11-10 23:09:18 +0900

    Headerのプロップスを作成

--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -1,16 +1,25 @@
+'use client'
+
+import type { HeaderProps } from '@/consts/header'
 import { Avatar, AvatarFallback } from '@radix-ui/react-avatar'
 import { Menu } from 'lucide-react'
 import { Button } from '../shadcn/button'
 
-const Header = () => {
+const Header: React.FC<HeaderProps> = ({
+  handleSidebarOpen,
+  headerTitle,
+  userName,
+  // TODO:ログイン機能が実装されたときに使用
+  setUserName,
+}) => {
   return (
-    <header className='h-14 w-full bg-red-900 flex items-center justify-between px-8'>
-      <Button variant='outline'>
+    <header className='h-14 w-full bg-red-900 flex items-center justify-between px-4'>
+      <Button variant='outline' onClick={handleSidebarOpen}>
         <Menu className='text-white' />
       </Button>
-      <h1 className='text-white font-bold text-xl'>シネマ予約</h1>
+      <h1 className='text-white font-bold text-xl'>{headerTitle}</h1>
       <Avatar>
-        <AvatarFallback>CN</AvatarFallback>
+        <AvatarFallback className='text-white'>{userName}</AvatarFallback>
       </Avatar>
     </header>
   )
---
+++ frontend/consts/meta.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'シネマ予約アプリ',
+  description: 'このアプリは映画の予約ができるアプリです。',
+}
