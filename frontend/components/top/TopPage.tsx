a4d6253a263edf98ef637ad3c5ff6001bb6bf996
TaiyoMaruyama <stickytomatobaby0501@gmail.com>
2024-11-14 20:45:40 +0900

    映画の画像をスライドできるUIを作成

--- frontend/components/top/TopPage.tsx
+++ frontend/components/top/TopPage.tsx
@@ -1,9 +1,23 @@
+import { demoAlphabetList } from '@/consts/reservation'
 import TextLabel from '../gadgets/TextLabel'
+import { Carousel, CarouselContent, CarouselItem } from '../shadcn/carousel'
 
 const TopPage = () => {
   return (
-    <div>
+    <div className='container'>
       <TextLabel text='公開中の映画' />
+      <div>
+        <Carousel>
+          <CarouselContent>
+            {demoAlphabetList.map((alpha) => (
+              <CarouselItem key={alpha} className='basis-1/5'>
+                <img src='https://picsum.photos/150/300' alt='' />
+                <p>{alpha}_movie_title</p>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div>
       <TextLabel text='公開予定の映画' />
       <TextLabel text='人気ランキング' />
     </div>
